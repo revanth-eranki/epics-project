6cebb80d151072f87f5ab3236780ec372e1fc74e
papergrid <seventhhokagedattebayo@gmail.com>
2024-02-11 00:27:01 +0530

    Added templated html, css for regration of user and safe authentication and minor changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-
 from . import views 
 
-
 urlpatterns = [
-
     path('register',views.register,name = "register"),
-
     path('login',views.login_page,name="login"),
-
     path('logout',views.logout_page,name='logout')
-
 ]
