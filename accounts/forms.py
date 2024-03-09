ceb00e9ac15072a412e79d1335e133afbf3749c5
papergrid <seventhhokagedattebayo@gmail.com>
2024-03-09 16:25:56 +0530

    Added forms for registration adn login

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,11 +7,21 @@ class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class']='form-field'
+        self.fields['password'].widget.attrs['class']='form-field'
+
+        self.fields['username'].widget.attrs['placeholder']='Username'
+        self.fields['password'].widget.attrs['placeholder']='Password'
+
+
 class RegisterForm(UserCreationForm):
 
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control','id':'input-first_name'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control', 'id':'input-last_name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'id':'input-email'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-field','id':'input-first_name', 'placeholder':'First Name'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-field', 'id':'input-last_name', 'placeholder':'Last Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-field', 'id':'input-email', 'placeholder':'Email'}))
     
     class Meta:
         model=User
@@ -20,10 +30,14 @@ class RegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(RegisterForm, self).__init__(*args, **kwargs)
 
-        self.fields['username'].widget.attrs['class']='form-control'
-        self.fields['password1'].widget.attrs['class']='form-control'
-        self.fields['password2'].widget.attrs['class']='form-control'
+        self.fields['username'].widget.attrs['class']='form-field'
+        self.fields['password1'].widget.attrs['class']='form-field'
+        self.fields['password2'].widget.attrs['class']='form-field'
 
         self.fields['username'].widget.attrs['id']='input-username'
         self.fields['password1'].widget.attrs['id']='input-password1'
         self.fields['password2'].widget.attrs['id']='input-password2'
+
+        self.fields['username'].widget.attrs['placeholder']='Username'
+        self.fields['password1'].widget.attrs['placeholder']='Password'
+        self.fields['password2'].widget.attrs['placeholder']='Re-enter Password'
--- accounts/views.py
+++ accounts/views.py
@@ -11,21 +11,41 @@ def logout_page(request):
 
 def login_page(request):
 
-    if request.method=='POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    # if request.method=='GET':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
 
-        user = authenticate(username=username,password=password)
+    #     user = authenticate(username=username,password=password)
 
-        if user is not None:
-            login(request,user)
-            return redirect('/')
-        else: 
-            messages.info(request,'Invalid credentials')
-            return redirect('login')
+    #     if user is not None:
+    #         login(request,user)
+    #         return redirect('/')
+    #     else: 
+    #         messages.info(request,'Invalid credentials')
+    #         return redirect('login')
 
-    else: 
-        return render(request,'login.html')
+    # else: 
+    #     return render(request,'login.html')
+
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request,'login.html', {'form': form})
+    
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                login(request, user)
+                return redirect('/')
+        
+        # form is not valid or user is not authenticated
+        messages.error(request,f'Invalid username or password')
+        return render(request,'login.html',{'form': form})
+
 
 def register(request):
     # if request.method=='POST':
@@ -55,4 +75,16 @@ def register(request):
     
     if request.method == 'GET':
         form = RegisterForm()
-        return render(request, 'register.html', {'form': form}) 
+        return render(request, 'register.html', {'form': form})    
+   
+    if request.method == 'POST':
+        form = RegisterForm(request.POST) 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have signed up successfully.')
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'register.html', {'form': form})
