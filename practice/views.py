5fbb3406cb6617ab4ea613202a4dabe16d974569
papergrid <seventhhokagedattebayo@gmail.com>
2024-03-12 22:51:03 +0530

    Added working tabbed navigation for results

--- practice/views.py
+++ practice/views.py
@@ -3,20 +3,20 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
-@login_required(login_url='accounts/login')
+@login_required(login_url='login')
 def home (request):
     return render(request,'index.html')
 
-@login_required(login_url='accounts/login')
+@login_required(login_url='login')
 def profile(request):
 
-    object = get_object_or_404(Profile, pk='422101')
+    object = get_object_or_404(Profile, pk='422180')
     return render(request,'profile.html',{'object': object})
 
-@login_required(login_url='accounts/login')
+@login_required(login_url='login')
 def result(request):
     return render(request,'result.html')
 
-@login_required(login_url='accounts/login')
+@login_required(login_url='login')
 def sem(request):
     return render(request,'sem.html')
